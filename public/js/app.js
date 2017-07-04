bc328b1b6ae04685a330073d4c8faeeedaf25ed5
Alex <Alex@Ralfs-MBP-3.fritz.box>
2017-07-04 23:00:50 +0200

    online monitoring

---
+++ public/js/app.js
@@ -0,0 +1,18 @@
+$(function () {
+    setInterval(function () {
+        console.log('int');
+        check()
+    }, 1000);
+
+    function check() {
+        console.log('checking');
+        $.get('http://127.0.0.1:3000/workerstatus', {}, function (data) {
+            console.log('get');
+            obj = JSON.parse(data);
+
+            $('.mpm_status').html(obj.mpm.toString() + ' messages per minute');
+            $('.worker_status').html(obj.workerconn + '/' + obj.workercount + ' worker sending');
+            $('.small_status').html(obj.reconnecting + ' reconnecting');
+        });
+    }
+});
